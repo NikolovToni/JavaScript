419d8d352765b24814a92f9ce921790b1ef58b7f
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-12 15:40:52 +0300

    Task solved

--- BasicSyntax/05.WeatherForecast.js
+++ BasicSyntax/05.WeatherForecast.js
@@ -1,5 +1,7 @@
-function solve() {
-
+function solve(town, degrees) {
+console.log("Today in " + town + " it is " + degrees + " degrees.");
 }
 
-solve();
+solve("Sofia", 29);
+solve("Varna", 32);
+solve("Plovdiv", 18);
