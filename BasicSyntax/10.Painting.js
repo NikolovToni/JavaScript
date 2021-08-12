f8c535532ac031cc4104aeed3390c57cfdf045db
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-12 16:45:47 +0300

    Task solved

--- BasicSyntax/10.Painting.js
+++ BasicSyntax/10.Painting.js
@@ -1,5 +1,14 @@
-function solve() {
+function solve(yellow) {
+    let red = yellow / 4;
+    let white = yellow * 2;
+    let total = red + yellow + white;
 
+    console.log("Red: " + red);
+    console.log("Yellow: " + yellow);
+    console.log("White: " + white);
+    console.log("Total: " + total);    
 }
 
-solve();
+solve(10);
+solve(17);
+solve(42);
