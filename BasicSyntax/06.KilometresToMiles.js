b473c5404564e3d2469091af2067b31711026847
Toni Nikolov <nikolov.s.toni@gmail.com>
2021-08-12 15:44:50 +0300

    Task solved

--- BasicSyntax/06.KilometresToMiles.js
+++ BasicSyntax/06.KilometresToMiles.js
@@ -1,5 +1,10 @@
-function solve() {
-    
+function solve(km) {
+    let mile = 0.621371;
+    let result = km * mile;
+    console.log(result);
 }
 
-solve();
+solve(5);
+solve(10.5);
+solve(42);
+solve(123.5);
