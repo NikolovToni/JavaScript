88e8f97dc5e0920044e6bd7e5ea64b918d06186f
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-16 14:54:40 +0300

    Task solved

---
+++ ConditionalConstructions/03.Number0To9.js
@@ -0,0 +1,39 @@
+function solve(number) {
+    switch (number) {
+        case 0:
+            console.log("Zero");
+            break;
+        case 1:
+            console.log("One");
+            break;
+        case 2:
+            console.log("Two");
+            break;
+        case 3:
+            console.log("Three");
+            break;
+        case 4:
+            console.log("Four");
+            break;
+        case 5:
+            console.log("Five");
+            break;
+        case 7:
+            console.log("Seven");
+            break;
+        case 8:
+            console.log("Eight");
+            break;
+        case 9:
+            console.log("Nine");
+            break;
+        default:
+            console.log("Too big");
+            break;
+    }
+}
+
+solve(7);
+solve(5);
+solve(9);
+solve(10);
