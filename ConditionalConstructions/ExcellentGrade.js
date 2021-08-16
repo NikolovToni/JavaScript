6c80e5782bb52466a08e552786b61e85fd657dbb
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-16 14:44:45 +0300

    Task solved

---
+++ ConditionalConstructions/ExcellentGrade.js
@@ -0,0 +1,12 @@
+function solve(grade) {
+    if (grade >= 5.5) {
+        console.log("Excellent!");
+    }
+    else {
+        console.log("None");
+    }
+}
+
+solve(6);
+solve(5);
+solve(5.51);
