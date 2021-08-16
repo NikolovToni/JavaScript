641d344b8723a474be616bf74864aeb04110e003
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-16 16:08:13 +0300

    Task solved

---
+++ Loops/06.SumOfVowels.js
@@ -0,0 +1,23 @@
+function solve(str) {
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == 'a') {
+            sum += 1;
+        } else if (str[i] == 'e') {
+            sum += 2;
+        } else if (str[i] == 'i') {
+            sum += 3;
+        } else if (str[i] == 'o') {
+            sum += 4;
+        } else if (str[i] == 'u') {
+            sum += 5;
+        }
+    }
+
+    console.log(sum);
+}
+
+solve("Hello");
+solve("hi");
+solve("bye");
+solve("zzzz");
