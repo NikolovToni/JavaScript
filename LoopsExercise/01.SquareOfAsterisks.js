d002b6daffe98a08cdc8957e162ac957641b60ea
NikolovToni <nikolov.s.toni@gmail.com>
2021-08-16 20:39:29 +0300

    Initial commit

--- LoopsExercise/01.SquareOfAsterisks.js
+++ LoopsExercise/01.SquareOfAsterisks.js
@@ -1,5 +1,10 @@
-function solve() {
-    
+function solve(number) {
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            process.stdout.write("*");
+        }        
+        console.log("");
+    }
 }
 
-solve();
+solve(3);
